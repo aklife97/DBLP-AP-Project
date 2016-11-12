6c78f164816ff4579d07d2edee1ad8eeb2ff8b41
aklife97 <aklife97@gmail.com>
2016-11-13 00:43:41 +0530

    handler fixed

--- Database.java
+++ Database.java
@@ -21,41 +21,72 @@ public class Database{
 	}
 }
 class Handler extends DefaultHandler{
-	private boolean bArticle = false;
-	private boolean bInproceedings = false;
-	private boolean bProceedings = false;
-	private boolean bBook = false;
-	private boolean bIncollection = false;
-	private boolean bPHDThesis = false;
-	private boolean bMastersThesis = false;
-	private boolean bWWW = false;
-	private HashMap<String, boolean> pubs = new HashMap<String, boolean>();
-	public Handler(){
-		pubs.put("article",bArticle);
-		pubs.put("inproceedings", bInproceedings);
-		pubs.put("proceedings", bProceedings);
-		pubs.put("book", bBook);
-		pubs.put("incollection", bIncollection);
-		pubs.put("phdthesis", bPHDThesis);
-		pubs.put("mastersthesis", bMastersThesis);
-		pubs.put("www", bWWW);
-	}
+	private boolean bAuthor = false;
+	private boolean bTitle = false;
+	private boolean bPages = false;
+	private boolean bYear = false;
+	private boolean bVolume = false;
+	private boolean bJournal = false;
+	private boolean bBookTitle = false;
+	private boolean bURL = false;
 	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException{
-		// if (qName.equalsIgnoreCase("article")){
-		for (String key : pubs.keySet()){
-			if (qName.equalsIgnoreCase(key)){
-				pubs.get(key) = true;
-			}
+		if (qName.equalsIgnoreCase("author")){
+			bAuthor = true;
 		}
-	}
-	public void endElement(String uri, String localName, String qName) throws SAXException{
-		for (String key : pubs.keySet()){
-			if (qName.equalsIgnoreCase(key)){
-				pubs.get(key) = true;
-			}
+		if (qName.equalsIgnoreCase("title")){
+			bTitle = true;
+		}
+		if (qName.equalsIgnoreCase("pages")){
+			bPages = true;
+		}
+		if (qName.equalsIgnoreCase("year")){
+			bYear = true;
+		}
+		if (qName.equalsIgnoreCase("volume")){
+			bVolume = true;
+		}
+		if (qName.equalsIgnoreCase("journal")){
+			bJournal = true;
+		}
+		if (qName.equalsIgnoreCase("booktitle")){
+			bBookTitle = true;
+		}
+		if (qName.equalsIgnoreCase("url")){
+			bURL = true;
 		}
 	}
 	public void characters(char c[], int start, int length) throws SAXException{
-		
+		if (bAuthor){
+			new String(ch, start, length);
+			bAuthor = false;
+		}
+		else if (bTitle){
+			new String(ch, start, length);
+			bTitle = false;
+		}
+		else if (bPages){
+			new String(ch, start, length);
+			bPages = false;
+		}
+		else if (bYear){
+			new String(ch, start, length);
+			bYear = false;
+		}
+		else if (bVolume){
+			new String(ch, start, length);
+			bVolume = false;
+		}
+		else if (bJournal){
+			new String(ch, start, length);
+			bJournal = false;
+		}
+		else if (bBookTitle){
+			new String(ch, start, length);
+			bBookTitle = false;
+		}
+		else if (bURL){
+			new String(ch, start, length);
+			bURL = false;
+		}
 	}
 }
