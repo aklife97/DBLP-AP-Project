513015f3bfc6f36dbac426367eb4ffbd57240f2a
aklife97 <aklife97@gmail.com>
2016-11-12 23:51:01 +0530

    added basic handler

--- Database.java
+++ Database.java
@@ -21,5 +21,41 @@ public class Database{
 	}
 }
 class Handler extends DefaultHandler{
-	
+	private boolean bArticle = false;
+	private boolean bInproceedings = false;
+	private boolean bProceedings = false;
+	private boolean bBook = false;
+	private boolean bIncollection = false;
+	private boolean bPHDThesis = false;
+	private boolean bMastersThesis = false;
+	private boolean bWWW = false;
+	private HashMap<String, boolean> pubs = new HashMap<String, boolean>();
+	public Handler(){
+		pubs.put("article",bArticle);
+		pubs.put("inproceedings", bInproceedings);
+		pubs.put("proceedings", bProceedings);
+		pubs.put("book", bBook);
+		pubs.put("incollection", bIncollection);
+		pubs.put("phdthesis", bPHDThesis);
+		pubs.put("mastersthesis", bMastersThesis);
+		pubs.put("www", bWWW);
+	}
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException{
+		// if (qName.equalsIgnoreCase("article")){
+		for (String key : pubs.keySet()){
+			if (qName.equalsIgnoreCase(key)){
+				pubs.get(key) = true;
+			}
+		}
+	}
+	public void endElement(String uri, String localName, String qName) throws SAXException{
+		for (String key : pubs.keySet()){
+			if (qName.equalsIgnoreCase(key)){
+				pubs.get(key) = true;
+			}
+		}
+	}
+	public void characters(char c[], int start, int length) throws SAXException{
+		
+	}
 }
