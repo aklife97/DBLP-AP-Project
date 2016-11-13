aa9beaacb7f5c195585ae0da13ca6665b2bb8de8
Tushar arora <arora.tushar1208@gmail.com>
2016-11-14 02:10:31 +0530

    added action listeners and drop down box to initframe

--- GUI.java
+++ GUI.java
@@ -12,6 +12,7 @@ public class GUI
 	private JPanel sidePanel;
 	private JPanel displayPanel;
 	private JComboBox<String> queries;
+	private JButton submit,reset;
 	private int flag=0;
 
 	public GUI()
@@ -27,6 +28,10 @@ public class GUI
 		mainFrame.setResizable(false);
 		mainFrame.setLayout(null);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		submit=new JButton("Submit");
+		reset=new JButton("Reset");
+		submit.setBackground(Color.BLACK);
+		reset.setBackground(Color.RED);
 		//---
 		sidePanel=new JPanel();
 		sidePanel.setPreferredSize(new Dimension(250,380));
@@ -36,7 +41,26 @@ public class GUI
 		sidePanel.setLayout(null);
 		sidePanel.setBorder(BorderFactory.createLineBorder(Color.black,2));
 		sidePanel.setBackground(Color.WHITE);	
+		queries = new JComboBox<String>();
+		queries.addItem("Queries");
+		queries.addItem("Query 1");
+		queries.addItem("Query 2");
+		queries.addItem("Query 3");
+		sidePanel.add(queries);
+		queries.setBounds(50,100,100,20);
+		queries.setAlignmentX(Component.CENTER_ALIGNMENT);
 		//---
+		displayPanel=new JPanel();
+		displayPanel.setPreferredSize(new Dimension(450,380));
+		displayPanel.setMinimumSize(new Dimension(450,380));
+		displayPanel.setMaximumSize(new Dimension(450,380));
+		displayPanel.setLocation(250,120);
+		displayPanel.setLayout(null);
+		displayPanel.setBorder(BorderFactory.createLineBorder(Color.black,2));
+		displayPanel.setBackground(Color.WHITE);
+		JLabel random = new JLabel("Results Here");
+		random.setBounds(150,100,150,40);
+		displayPanel.add(random);	
 		//---
 		upperPanel=new JPanel();
 		upperPanel.setSize(new Dimension(700,120));
@@ -63,6 +87,20 @@ public class GUI
 		mainFrame.add(displayPanel);
 		mainFrame.setVisible(true); 
 		//---
+		queries.addActionListener(new ActionListener() {
+		    @Override
+		    public void actionPerformed(ActionEvent event) {
+		        JComboBox<String> q = (JComboBox<String>) event.getSource();
+		        String selectedQuery = (String) q.getSelectedItem();
+		        if (selectedQuery.equals("Query 1")) {
+		            setQuery1();
+		        }     else if (selectedQuery.equals("Query 2")) {
+		            setQuery2();
+		        } else if(selectedQuery.equals("Query 3")) {
+		        	setQuery3();
+		        }
+		    }
+		});
 	}
 
 	public static void main(String[] args)
