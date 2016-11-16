db3cb0fe5abbf8644717c3fcba60247aa1942dff
aklife97 <aklife97@gmail.com>
2016-11-16 22:46:27 +0530

    added Author

---
+++ Author.java
@@ -0,0 +1,12 @@
+/**
+*@author Abhinav Khattar 2015120, Tushar Arora 2015107
+*/
+public class Author{
+	private String name;
+	public Author(String _name){
+		name = _name;
+	}
+	public String getName(){
+		return name;
+	}
+}
