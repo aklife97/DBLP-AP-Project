052b4f1e9299572730e0876b64ac83287ad2501b
aklife97 <aklife97@gmail.com>
2016-11-19 18:23:35 +0530

    added Query2

---
+++ Query2.java
@@ -0,0 +1,14 @@
+/**
+*@author Abhinav Khattar 2015120, Tushar Arora 2015107
+*/
+public class Query2{
+	private Database dbase;
+	private int k;
+	public Query2(String filename, int _k){
+		k = _k;
+		dbase = new Database(filename);
+	}
+	public check(DataRecords record){
+
+	}
+}
